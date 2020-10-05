3fdf13c418466996d126dc255ff592fcc71c64fd
laysan <laysa@LAPTOP-2RQKGOVQ>
2020-10-05 21:48:55 +0300

    Сделала аутентификацию и авторизацию на куки. Взяла с метанита. Стили нормально перенести не получилось :( Только на Login, но не на чат комнату. Регистрация есть только в виде представления, в бд не уходит. Данные для бд в AccountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using ChatApp_SignalR_.Models;
+using Sitecore.FakeDb;
+//using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Internal;
+
+namespace ChatApp_SignalR_.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ApplicationContext _context;
+        public AccountController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        // тестирование SignalR
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _context.Users
+                    .Include(u => u.Role)
+                    .FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(user); // аутентификация
+                    return RedirectToAction("Index", "Account");// переадресация на метод Index
+                }
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+        private async Task Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role?.Name)
+            };
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+    }
+}
--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,10 +1,24 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ChatApp_SignalR_.Hubs{
-    public class ChatHub:Hub{
-        public async Task Send(string message){
-            await Clients.All.SendAsync("ReceiveMessage",message);
+    //public class ChatHub:Hub{
+    //    public async Task Send(string message){
+    //        await Clients.All.SendAsync("ReceiveMessage",message);
+    //    }
+    //}
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await Clients.All.SendAsync("Receive", message, userName);
+        }
+        [Authorize(Roles = "admin")]
+        public async Task Notify(string message, string userName)
+        {
+            await Clients.All.SendAsync("Receive", message, userName);
         }
     }
 }
